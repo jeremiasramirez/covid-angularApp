e93d6036e9c2b58327fc6251a448d995bdec5b78
jeremias ramirez <jereramirez499@gmail.com>
2020-06-12 21:26:26 -0400

    comenzando a desplegar datos de casos globales en home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { Summary  } from '../services/summary.service'
+import { Feature  } from '../services/feature.service'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   providers: [
-    Summary
+    Summary,
+    Feature
   ]
 })
+
 export class HomeComponent implements OnInit {
 
+  public globalData : SummaryData;
+
+  public spinnerChange :spinner ={
+    off:true
+  } 
+
+
   constructor(public summary:Summary) { 
-    this.summary.get().subscribe((data)=>console.log(data))
+    this.summary.get().subscribe((data:SummaryData)=>{
+      this.globalData=data
+      console.log(this.globalData);
+      
+    })
+    
   }
 
   ngOnInit() {
-  }
+  } 
+  
+}
 
+interface spinner {
+  off:boolean;
+}
 
-
+interface SummaryData {
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number
 }
